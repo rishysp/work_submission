08553a80882e6a23ccabfe504760190cc88b650d
Rishy Parasar <58654138+rishysp@users.noreply.github.com>
2021-07-14 00:10:52 +0530

    Add files via upload
    This application fetches the data from the database and shows that in a tabular form.

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
+import yaml
+
+app = Flask(__name__)
+
+# Configure db
+db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+
+mysql = MySQL(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+
+        
+        return redirect('/users')
+    return render_template('index.html')
+
+@app.route('/users')
+def users():
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("SELECT * FROM world_happiness_report_2021")
+    if resultValue > 0:
+        userDetails = cur.fetchall()
+        return render_template('data.html',userDetails=userDetails)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
